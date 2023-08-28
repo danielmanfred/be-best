e466b2d92085aacf86c98832844cde2603ed0d7b
Daniel Brito <danielmanfred@gmail.com>
2023-08-28 10:31:39 -0300

    feat: add MongoClient with mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,26 @@ import { config } from "dotenv";
 import { GetPersonController } from "./application/controllers/get-person/get-person-controller";
 import { GetPerson } from "./application/usercases/get-person/get-person";
 import { MongoPersonRepository } from "./application/repositories/person-repository/mongo-person-repository";
+import { MongoClient } from "./application/database/mongo";
 
-config();
+const main = async () => {
+  config();
+  const app = express();
+  await MongoClient.connect();
 
-const app = express();
-
-const port = process.env.PORT || 8000;
-
-app.listen(port, () => console.log(`listening on port ${port}!`));
-
-app.get("/person", async (req, res) => {
-  const personRepository = new MongoPersonRepository();
-  const getPerson = new GetPerson(personRepository);
-  const getPersonController = new GetPersonController(getPerson);
-  const response = await getPersonController.handle({
-    firstName: "Daniel",
-    lastName: "Brito",
+  app.get("/person", async (req, res) => {
+    const personRepository = new MongoPersonRepository();
+    const getPerson = new GetPerson(personRepository);
+    const getPersonController = new GetPersonController(getPerson);
+    const response = await getPersonController.handle({
+      firstName: "Daniel",
+      lastName: "Brito",
+    });
+    res.status(response.statusCode).send(response.body);
   });
-  res.status(response.statusCode).send(response.body);
-});
+
+  const port = process.env.PORT || 8000;
+  app.listen(port, () => console.log(`listening on port ${port}!`));
+};
+
+main();
